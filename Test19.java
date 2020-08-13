db4958925b8a2120cbf0d7b2fca8ef3f726eee5c
daseulkim <merilynkim37@gmail.com>
2020-08-13 14:32:03 +0900

    step2

---
+++ Test19.java
@@ -0,0 +1,11 @@
+Public class Test19{
+public static void main(string[] args)}{
+	int score =90;
+	String result="";
+	if(score>+60)
+		result="pass";
+	else
+		result="fail";
+	system.out.println(result);
+}
+}
